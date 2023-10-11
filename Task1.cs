36339ca89144866c543b390ea1fb4a09310b0576
Murad <145739695+Murgitadd@users.noreply.github.com>
2023-10-12 03:02:33 +0400

    Done Task1.cs

--- Task1.cs
+++ Task1.cs
@@ -1,10 +1,49 @@
-Console.WriteLine("Enter a four-digit number:");
+using System;
 
-int num = int.Parse(Console.ReadLine());
+class Employee
+{
+    public static string Name { get; set; } = "Murad";
+    public static double Salary { get; set; } = 500;
+    public bool IsSuccessful { get; set; }
 
-int minlik = num / 1000;
-int yuzluk = (num % 1000) / 100;
-int onluq = (num % 100) / 10;
-int teklik = (num % 10);
+    public void GetFeedback()
+    {
+        Console.WriteLine("Did you like the employee? (Enter 'true' or 'false')");
+        if (bool.TryParse(Console.ReadLine(), out bool liked))
+        {
+            IsSuccessful = liked;
 
-Console.WriteLine(minlik+yuzluk+onluq+teklik);
+            if (IsSuccessful)
+            {
+                Salary += 100;
+                Console.WriteLine("Employee's salary increased by 100.");
+            }
+            else
+            {
+                Console.WriteLine("Employee's salary remains the same.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid input for feedback. Employee's salary remains the same.");
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine($"Employee: {Employee.Name}");
+        Console.WriteLine($"Salary: {Employee.Salary}");
+
+        Employee employee = new Employee();
+        employee.GetFeedback();
+
+        Console.WriteLine($"Updated Salary: {Employee.Salary}");
+        Console.WriteLine($"Is Successful: {employee.IsSuccessful}");
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
